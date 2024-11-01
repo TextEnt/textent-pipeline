463ead2a6e72d4fbd51325a316435ccc63e53b07
Pierre Kunzli <pierre.kuenzli@unige.ch>
2024-11-01 10:40:12 +0100

    some refactoring

--- scripts/download-books.py
+++ scripts/download-books.py
@@ -2,6 +2,40 @@ import requests
 import argparse
 import os
 
+def download_books(output_folder, urls):
+    # create a directory for each book
+    # manage 429 codes (too many requests)
+    for url in urls:
+        book_name = url.split('/')[-1]
+        try:
+            os.mkdir(output_folder + '/' + book_name)
+        except FileExistsError:
+            print("Folder " + output_folder + '/' + book_name + " already exists.")
+        cpt = 1
+        current_url = url+"/f"+str(cpt)+".highres"
+        print("Downloading " + current_url + "...")
+        image = requests.get(current_url)
+        new_url = url+"/f"+str(cpt+1)+".highres"
+        print("Downloading " + new_url + "...")
+        new_image = requests.get(new_url)
+        # no proper error code, must download until content stay the same
+        while image.content != new_image.content:
+            with open(output_folder + '/' + book_name + '/' + str(cpt) + '.jpg', 'wb') as f:
+                f.write(image.content)
+            cpt += 1
+            image = new_image
+            new_url = url+"/f"+str(cpt+1)+".highres"
+            print("Downloading " + new_url + "...")
+            new_image = requests.get(new_url)
+        with open(output_folder + '/' + book_name + '/' + str(cpt) + '.jpg', 'wb') as f:
+                f.write(image.content)
+
+def get_books_urls(input_metadata):
+    # extract the last token of each line as URL except first line
+    with open(input_metadata, 'r') as metadata_file:
+        urls = list(set([line.split()[-1] for line in metadata_file.readlines()[1:]]))
+    return urls
+
 parser = argparse.ArgumentParser(description='Download books from a metadata file.')
 parser.add_argument('input', metavar='IN', type=str, 
                     help='source metadata file')
@@ -9,38 +43,10 @@ parser.add_argument('output', metavar='OUT', type=str,
                     help='folder where files are stored')
 args = parser.parse_args()
 
-# extract the last token of each line as URL except first line
-with open(args.input, 'r') as metadata_file:
-    urls = list(set([line.split()[-1] for line in metadata_file.readlines()[1:]]))
-
+urls = get_books_urls(args.input)
 try:
     os.mkdir(args.output)
 except FileExistsError:
     print("Folder " + args.output + " already exists.")
+download_books(args.output, urls)
 
-# create a directory for each book
-# manage 429 codes (too many requests)
-for url in urls:
-    book_name = url.split('/')[-1]
-    try:
-        os.mkdir(args.output + '/' + book_name)
-    except FileExistsError:
-        print("Folder " + args.output + '/' + book_name + " already exists.")
-    cpt = 1
-    current_url = url+"/f"+str(cpt)+".highres"
-    print("Downloading " + current_url + "...")
-    image = requests.get(current_url)
-    new_url = url+"/f"+str(cpt+1)+".highres"
-    print("Downloading " + new_url + "...")
-    new_image = requests.get(new_url)
-    # no proper error code, must download until content stay the same
-    while image.content != new_image.content:
-        with open(args.output + '/' + book_name + '/' + str(cpt) + '.jpg', 'wb') as f:
-            f.write(image.content)
-        cpt += 1
-        image = new_image
-        new_url = url+"/f"+str(cpt+1)+".highres"
-        print("Downloading " + new_url + "...")
-        new_image = requests.get(new_url)
-    with open(args.output + '/' + book_name + '/' + str(cpt) + '.jpg', 'wb') as f:
-            f.write(image.content)
