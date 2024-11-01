1e1ae9ecbdeb97c7f891cd0de0aaf2c5079c29e7
Pierre Kunzli <pierre.kuenzli@unige.ch>
2024-11-01 11:41:52 +0100

    wip gestion d'erreur

--- scripts/download-books.py
+++ scripts/download-books.py
@@ -2,6 +2,15 @@ import requests
 import argparse
 import os
 
+def download_one_page(url):
+    image = requests.get(url)
+    if image.status_code == 429:
+        print("Too many requests")
+        print(image.headers)
+        exit(0)
+    return image
+
+
 def download_books(output_folder, urls):
     # create a directory for each book
     # manage 429 codes (too many requests)
@@ -14,10 +23,10 @@ def download_books(output_folder, urls):
         cpt = 1
         current_url = url+"/f"+str(cpt)+".highres"
         print("Downloading " + current_url + "...")
-        image = requests.get(current_url)
+        image = download_one_page(current_url)
         new_url = url+"/f"+str(cpt+1)+".highres"
         print("Downloading " + new_url + "...")
-        new_image = requests.get(new_url)
+        new_image = download_one_page(new_url)
         # no proper error code, must download until content stay the same
         while image.content != new_image.content:
             with open(output_folder + '/' + book_name + '/' + str(cpt) + '.jpg', 'wb') as f:
@@ -26,7 +35,7 @@ def download_books(output_folder, urls):
             image = new_image
             new_url = url+"/f"+str(cpt+1)+".highres"
             print("Downloading " + new_url + "...")
-            new_image = requests.get(new_url)
+            new_image = download_one_page(new_url)
         with open(output_folder + '/' + book_name + '/' + str(cpt) + '.jpg', 'wb') as f:
                 f.write(image.content)
 
