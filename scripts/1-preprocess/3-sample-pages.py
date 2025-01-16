145ded41dd71c82a8a15b3debc600218c23d5faa
Pierre Kunzli <pierre.kuenzli@unige.ch>
2025-01-16 13:07:34 +0100

    start sampling script

--- scripts/1-preprocess/3-sample-pages.py
+++ scripts/1-preprocess/3-sample-pages.py
@@ -3,6 +3,8 @@
 # and store them in a new folder
 
 import argparse
+import random
+import os
 
 parser = argparse.ArgumentParser(description='Extract pages from bookks folder.')
 parser.add_argument('input', metavar='IN', type=str, 
@@ -14,7 +16,6 @@ parser.add_argument('number', metavar='NUM', type=int,
 args = parser.parse_args()
 
 # get the number of books in source folder
-import os
 books = os.listdir(args.input)
 n_books = len(books)
 # compute every nth book to extract
@@ -22,7 +23,6 @@ step = max(n_books // args.number, 1)
 for i in range(0, n_books, step):
     book = books[i]
     # copy one random page from the book to the destiation folder
-    import random
     pages = os.listdir(args.input + '/' + book)
     page = random.choice(pages)
     os.system('cp ' + args.input + '/' + book + '/' + page + ' ' + args.output + '/' + book + '_' + page)
